8c4254949c11999e4ba457bd289fe8a71f5fbe28
DeepCoreB4 <cupparikun@gmail.com>
2023-04-30 21:57:41 +0200

    ..repo fix

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "DeepCoreB4";
-export const REPO = "DeepCore-ChatGPT-Next-Web";
+export const REPO = "DeepCore-ChatGPT-Next-Web-Main";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
